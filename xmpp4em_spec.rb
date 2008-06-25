8d61568214bd74dafae772bffb4a7ce41565df12
Aman Gupta <aman@tmm1.net>
2008-06-25 12:11:49 -0700

    fix spec to run with latest EM

--- xmpp4em_spec.rb
+++ xmpp4em_spec.rb
@@ -15,43 +15,49 @@ shared 'eventmachine' do
   end
 end
 
-describe 'XMPP4EM' do
-  behaves_like 'eventmachine'
+EM.run{
+  Thread.new{
+    describe 'XMPP4EM' do
+      behaves_like 'eventmachine'
 
-  @foo = XMPP4EM::Client.new('foo@localhost', 'test', :auto_register => true)
-  @bar = XMPP4EM::Client.new('bar@localhost', 'test', :auto_register => true)
+      @foo = XMPP4EM::Client.new('foo@localhost', 'test', :auto_register => true)
+      @bar = XMPP4EM::Client.new('bar@localhost', 'test', :auto_register => true)
 
-  should 'login to an xmpp server' do
-    @foo.on(:login) do
-      @foo.send Jabber::Presence.new
-      wake
-    end
+      should 'login to an xmpp server' do
+        @foo.on(:login) do
+          @foo.send Jabber::Presence.new
+          wake
+        end
 
-    @foo.connect
-    wait
+        @foo.connect
+        wait
 
-    @foo.should.be.connected?
-  end
-
-  should 'send messages to others' do
-    @bar.on(:login) do
-      @bar.send Jabber::Presence.new do
-        wake
+        @foo.should.be.connected?
       end
+
+      should 'send messages to others' do
+        @bar.on(:login) do
+          @bar.send Jabber::Presence.new do
+            wake
+          end
+        end
+
+        received = nil
+        @bar.on(:message) do |msg|
+          received = msg.first_element_text('//body')
+          wake
+        end
+
+        @bar.connect
+        wait
+
+        @foo.send_msg 'bar@localhost', 'hello'
+        wait
+
+        received.should == 'hello'
+      end
+
+      EM.stop_event_loop
     end
-
-    received = nil
-    @bar.on(:message) do |msg|
-      received = msg.first_element_text('//body')
-      wake
-    end
-
-    @bar.connect
-    wait
-
-    @foo.send_msg 'bar@localhost', 'hello'
-    wait
-
-    received.should == 'hello'
-  end
-end
+  }
+}
