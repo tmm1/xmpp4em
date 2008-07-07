69ea8792315b6cbb6776428bbd8fbae4d4ad0891
Aman Gupta <aman@tmm1.net>
2008-07-07 13:22:39 -0700

    fix stress test

--- stress_test.rb
+++ stress_test.rb
@@ -8,20 +8,20 @@ num = 1000 if num < 1
 
 EM.epoll
 
-num.times do |i|
-  p i
-  users[i] = XMPP4EM::Client.new("test_#{i}@localhost", 'test', :auto_register => true)
-  users[i].on(:login) do
-    connected += 1
-    p ['connected', i, "#{connected} of #{num}"]
-
-    if connected == num
-      p ['done', Time.now - started]
-      exit unless ARGV[1]
+EM.run{
+  num.times do |i|
+    p i
+    users[i] = XMPP4EM::Client.new("test_#{i}@localhost", 'test', :auto_register => true)
+    users[i].on(:login) do
+      connected += 1
+      p ['connected', i, "#{connected} of #{num}"]
+  
+      if connected == num
+        p ['done', Time.now - started]
+        EM.stop_event_loop
+      end
     end
+  
+    users[i].connect('localhost', 5222)
   end
-
-  users[i].connect
-end
-
-$em_reactor_thread.join
+}
